114a30f0eaec937b8f609247a6149583af14da37
dimsplendid <dimsplendid@gmail.com>
2021-10-15 00:19:20 +0800

    django learning 1014

--- catalog/models.py
+++ catalog/models.py
@@ -24,7 +24,7 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
-    lang = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    lang = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book"
@@ -48,65 +48,81 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse("book-detail", args=[str(self.id)])
 
-import uuid # Required for unique book instances
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = "Genre"
+
+
+import uuid  # Required for unique book instances
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Unique ID for this particular book across whole library",
+    )
+    book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
     LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
+        ("m", "Maintenance"),
+        ("o", "On loan"),
+        ("a", "Available"),
+        ("r", "Reserved"),
     )
 
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
         blank=True,
-        default='m',
-        help_text='Book availability',
+        default="m",
+        help_text="Book availability",
     )
 
     class Meta:
-        ordering = ['due_back']
+        ordering = ["due_back"]
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        return f"{self.id} ({self.book.title})"
 
 
 class Author(models.Model):
     """Model representing an author."""
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
 
     class Meta:
-        ordering = ['last_name', 'first_name']
+        ordering = ["last_name", "first_name"]
 
     def get_absolute_url(self):
         """Returns the url to access a particular author instance."""
-        return reverse('author-detail', args=[str(self.id)])
+        return reverse("author-detail", args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.last_name}, {self.first_name}'
+        return f"{self.last_name}, {self.first_name}"
+
 
 class Language(models.Model):
     """Model representing the language of books"""
+
     name = models.CharField(
-        max_length=100, 
-        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)"
+        max_length=100,
+        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)",
     )
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
