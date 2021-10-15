9914befa593579c212ca23e839f398d1219ed3fd
andrew.tseng 曾煒翔 <andrew.tseng@cminl.oa>
2021-10-15 20:26:45 +0800

    1015

--- catalog/views.py
+++ catalog/views.py
@@ -36,9 +36,13 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the context
-        context = super(BookListView, self).get_context_data(**kwargs)
-        # Create any data and add it to the context
-        context['some_data'] = 'This is just some data'
-        return context
+    context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
